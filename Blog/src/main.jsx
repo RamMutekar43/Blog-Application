d16613837a163fb3fde1da1abed1faa3fb24ee52
RamMutekar43 <152324887+RamMutekar43@users.noreply.github.com>
2025-01-25 23:44:45 +0530

    Blog

--- Blog/src/main.jsx
+++ Blog/src/main.jsx
@@ -8,7 +8,6 @@ import { Route, Router, RouterProvider, createBrowserRouter, createRoutesFromEle
 import Home from './pages/Home.jsx'
 import Login from './pages/Login.jsx'
 import CreatePost from './pages/CreatePost.jsx'
-// import Layout from './components/Layout.jsx'
 
 // const router = createBrowserRouter(
 //   createRoutesFromElements(
@@ -22,7 +21,6 @@ import CreatePost from './pages/CreatePost.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <RouterProvider router={router}/> */}
     <App/>
   </React.StrictMode>,
 )
